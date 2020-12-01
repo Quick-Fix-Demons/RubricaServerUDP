80382127ab708e346d6b3eace7b33e0c3fc76188
ok_google <renda855@gmail.com>
2020-12-01 17:47:04 +0100

    Aggiunto salvataggio, modifica e ricerca contatti.

---
+++ src/rubricaserverudp/Contatto.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rubricaserverudp;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Alessio
+ */
+public class Contatto implements Serializable {
+    private String nomeContatto;
+    private String numero;
+
+    public Contatto(String nomeContatto, String numero) {
+        this.nomeContatto = nomeContatto;
+        this.numero = numero;
+    }
+
+    public String getNomeContatto() {
+        return nomeContatto;
+    }
+
+    public void setNomeContatto(String nomeContatto) {
+        this.nomeContatto = nomeContatto;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+    
+}
--- src/rubricaserverudp/Main.java
+++ src/rubricaserverudp/Main.java
@@ -12,8 +12,10 @@ import java.net.InetAddress;
 import java.net.SocketException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -33,6 +35,8 @@ public class Main {
             byte[] ricevi = new byte[1024];
             DatagramPacket request = new DatagramPacket(ricevi, ricevi.length);
             
+            List<Contatto> contatti = new ArrayList<>();
+            
             while (true) {
                 socket.receive(request);
                 String[] ricevuto = new String(request.getData()).split(" - ");
@@ -41,12 +45,44 @@ public class Main {
                 String risposta = null;
                 switch(ricevuto[0]) {
                     case "0": /* Ricerca per nome */
+                        for(int i = 0; i<contatti.size(); i++) {
+                            if(ricevuto[1].equals(contatti.get(i).getNomeContatto())) {
+                                risposta = contatti.get(i).getNomeContatto() + " - " + contatti.get(i).getNumero();
+                                break;
+                            }
+                        }
+                        if(risposta == null) risposta = "Contatto non trovato.";
                         break;
                     case "1": /* Ricerca per numero */
+                        for(int i = 0; i<contatti.size(); i++) {
+                            if(ricevuto[1].equals(contatti.get(i).getNumero())) {
+                                risposta = contatti.get(i).getNomeContatto() + " - " + contatti.get(i).getNumero();
+                                break;
+                            }
+                        }
+                        if(risposta == null) risposta = "Contatto non trovato.";
                         break;
                     case "2": /* Aggiunta alla rubrica */
+                        contatti.add(new Contatto(ricevuto[1], ricevuto[2]));
+                        System.out.println("Contatto \"" + ricevuto[1] + "\" aggiunto con successo.");
                         break;
                     case "3": /* Modifica alla rubrica */
+                        for(int i = 0; i<contatti.size(); i++) {
+                            if(ricevuto[2].equals(contatti.get(i).getNomeContatto())) {
+                                Contatto old = contatti.get(i);
+                                contatti.remove(i);
+                                if(ricevuto[1].equals("0")) {
+                                    contatti.add(new Contatto(ricevuto[3], old.getNumero()));
+                                    risposta = "Contatto modificato con successo.";
+                                }
+                                else if(ricevuto[1].equals("1")) {
+                                    contatti.add(new Contatto(old.getNomeContatto(), ricevuto[3]));
+                                    risposta = "Contatto modificato con successo.";
+                                }
+                                else if(risposta == null) risposta = "Contatto non trovato.";
+                                break;
+                            }
+                        }
                         break;
                 }
                 DatagramPacket response = new DatagramPacket(risposta.getBytes(), risposta.getBytes().length, clientAddress, clientPort);
